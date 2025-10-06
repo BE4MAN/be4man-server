a6e444d63c5da480cfe7ab25b19cfb80a1826fa4
wonseok2877 <wonseok2877@gmail.com>
2025-10-06 18:50:36 +0900

    feat: CORS 전체 허용 설정

--- src/main/java/sys/be4man/global/config/SecurityConfig.java
+++ src/main/java/sys/be4man/global/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package sys.be4man.global.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,35 +18,52 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
-            // 공개 목록
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                        "/health"
+                // 공개 목록
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(
+                                        // 헬스 체크
+                                        "/api/health"
 
-                        // swagger
-                        , "/v3/api-docs/**"
-                        , "/swagger-ui/**"
-                        , "/swagger-ui.html"
-                        , "/swagger-resources/**"
-                        , "/webjars/**"
-                        , "/swagger-ui/index.html"
+                                        // 스웨거
+                                        , "/api-docs/**"
+                                        , "/v3/api-docs/**"
+                                        , "/swagger-ui/**"
+                                        , "/swagger-ui.html"
+                                        , "/swagger-ui/index.html"
+                                        , "/swagger-resources/**"
+                                        , "/webjars/**"
 
-                        // 인증 (login, logout)
+                                        // 로그인, 회원가입
 
-                        // h2 콘솔
+                                        //
 
-                        //
+                                ).permitAll()
+                                .anyRequest().authenticated()
 
-                    )
-                    .permitAll()
+                        // 인가 - 권한 분기 처리
 
-                    .anyRequest().authenticated()
-
-                // 인가 - 권한 분기 처리
-
-            );
+                );
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // 임시로 모든 도메인 허용. 추후에 특정 도메인만 허용
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
+
