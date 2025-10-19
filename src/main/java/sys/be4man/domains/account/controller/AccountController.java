507ebba21d9f4c23b50d14f17176b70b7feaaf81
wonseok2877 <wonseok2877@gmail.com>
2025-10-20 00:38:53 +0900

    refactor: 스웨거 API 명세 추가

--- src/main/java/sys/be4man/domains/account/controller/AccountController.java
+++ src/main/java/sys/be4man/domains/account/controller/AccountController.java
@@ -1,5 +1,12 @@
 package sys.be4man.domains.account.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +21,7 @@ import sys.be4man.domains.auth.dto.AccountPrincipal;
 /**
  * 계정 관련 API 컨트롤러
  */
+@Tag(name = "Account", description = "계정 API")
 @Slf4j
 @RestController
 @RequestMapping("/api/accounts")
@@ -25,6 +33,16 @@ public class AccountController {
     /**
      * 내 계정 정보 조회
      */
+    @Operation(summary = "내 계정 정보 조회", description = "현재 로그인한 사용자의 계정 정보를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(schema = @Schema(implementation = AccountInfoResponse.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "계정을 찾을 수 없음",
+                    content = @Content)
+    })
+    @SecurityRequirement(name = "Bearer Authentication")
     @GetMapping("/me")
     public ResponseEntity<AccountInfoResponse> getMyAccount(
             @AuthenticationPrincipal AccountPrincipal principal
